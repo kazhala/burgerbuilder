8ea0e2e4f096ff695b202697e009c929198720de
kevin zhuang <kevinzhuang@kevins-MacBook-Pro-3.local>
2019-08-27 15:57:25 +1000

    reorder the folder structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../hoc/Aux';
+import Aux from '../Aux/Aux';
 import './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
